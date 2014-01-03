29a4e703f3381e6b5d380cb7862f263bb2dc2eb7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-01-03 05:57:52 +0000

    start adding a schema generation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.graphdb/trunk@28771 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/graphdb/generations/install.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+schema generation installation.
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 1
+
+from zope.generations.generations import SchemaManager
+
+import zope.intid
+
+class _GraphDBSchemaManager(SchemaManager):
+	"""
+	A schema manager that we can register as a utility in ZCML.
+	"""
+	def __init__(self):
+		super(_GraphDBSchemaManager, self).__init__(
+												generation=generation,
+												minimum_generation=generation,
+												package_name='nti.graphdb.generations')
+def evolve(context):
+	# ### from IPython.core.debugger import Tracer; Tracer()()
+	main(context)
+
+def main(context):
+	conn = context.connection
+	root = conn.root()
+
+	dataserver_folder = root['nti.dataserver']
+	lsm = dataserver_folder.getSiteManager()
+	lsm.getUtility(zope.intid.IIntIds)
+
