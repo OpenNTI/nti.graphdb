1720734709e52968944a739bae5964edf596e00e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-02 19:52:32 +0000

    change listener
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.graphdb/trunk@38407 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/graphdb/subscribers.py
+++ src/nti/graphdb/subscribers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 """
-$Id$
+.. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
@@ -9,15 +9,16 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
-from zope.processlifetime import IDatabaseOpenedWithRoot
+
+from pyramid import events as pyramid_events
 
 from nti.dataserver import interfaces as nti_interfaces
 
 def onChange(datasvr, msg, target=None, broadcast=None, **kwargs):
 	pass
 
-@component.adapter(IDatabaseOpenedWithRoot)
-def _set_change_listener(database_event):
+@component.adapter(pyramid_events.ApplicationCreated)
+def _set_change_listener(event):
 	dataserver = component.queryUtility(nti_interfaces.IDataserver)
 	if dataserver is not None:
 		dataserver.add_change_listener(onChange)
