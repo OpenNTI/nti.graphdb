7c4a5804bf81929af0132964188e7ef6afdbe821
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-06-23 19:32:39 +0000

    also move queue and job
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.graphdb/trunk@41926 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/graphdb/async/job.py
+++ src/nti/graphdb/async/job.py
@@ -7,3 +7,6 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+import zope.deprecation
+zope.deprecation.moved('nti.async.job')
