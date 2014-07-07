f41249057b085f80e7000ca9eb0f4bc04c32b5c2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-07 19:49:38 +0000

    remove listener
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.graphdb/trunk@42734 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/graphdb/subscribers.py
+++ src/nti/graphdb/subscribers.py
@@ -7,19 +7,3 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-from zope import component
-
-from pyramid import events as pyramid_events
-
-from nti.dataserver import interfaces as nti_interfaces
-
-def onChange(datasvr, msg, target=None, broadcast=None, **kwargs):
-	pass
-
-@component.adapter(pyramid_events.ApplicationCreated)
-def _set_change_listener(event):
-	dataserver = component.queryUtility(nti_interfaces.IDataserver)
-	if dataserver is not None:
-		dataserver.add_change_listener(onChange)
-
