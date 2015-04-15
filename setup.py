8bcffa3b828aeaf06e9fb90301e51f463414ebcd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-15 15:31:01 +0000

    document deps
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.graphdb/trunk@63164 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,36 +4,58 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-    'console_scripts': [
-        "nti_graph_constructor = nti.graphdb.utils.constructor:main",
-    ],
+	'z3c.autoinclude.plugin': [
+		'target = nti.dataserver',
+	],
+	'console_scripts': [
+		"nti_graph_constructor = nti.graphdb.utils.constructor:main",
+	],
 }
 
+TESTS_REQUIRE = [
+	'nose',
+	'nose-timer',
+	'nose-pudb',
+	'nose-progressive',
+	'nose2[coverage_plugin]',
+	'pyhamcrest',
+	'zope.testing',
+	'nti.nose_traceback_info',
+	'nti.testing'
+]
+
 setup(
-    name='nti.graphdb',
-    version=VERSION,
-    author='Jason Madden',
-    author_email='jason@nextthought.com',
-    description="NTI graphdb",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='pyramid preference',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name='nti.graphdb',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI graphdb",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='pyramid preference',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        ],
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti'],
 	install_requires=[
 		'setuptools',
-        'nti.async',
-        'py2neo'
+		'py2neo',
+		'nti.async'
+	],
+	extras_require={
+		'test': TESTS_REQUIRE,
+	},
+	dependency_links=[
+		'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
+		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
 	],
 	entry_points=entry_points
 )
