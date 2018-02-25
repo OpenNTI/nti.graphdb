3541449dc7cd24e77d6b1a9a45081dcaabc034d5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-24 18:37:00 -0600

    pylint; remove code old code

--- src/nti/graphdb/neo4j/node.py
+++ src/nti/graphdb/neo4j/node.py
@@ -3,14 +3,12 @@
 """
 .. $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import interface
-
-from py2neo.types import remote
 
 from nti.graphdb.interfaces import IGraphNode
 
@@ -27,33 +25,33 @@ from nti.schema.field import SchemaConfigured
 
 from nti.schema.fieldproperty import createDirectFieldProperties
 
+logger = __import__('logging').getLogger(__name__)
+
+
 @WithRepr
 @EqHash('id')
 @interface.implementer(IGraphNodeNeo4j)
 class Neo4jNode(SchemaConfigured):
-	createDirectFieldProperties(IGraphNodeNeo4j)
+    createDirectFieldProperties(IGraphNodeNeo4j)
 
-	_v_neo = None
+    _v_neo = None
 
-	neo = alias('_v_neo')
+    neo = alias('_v_neo')
 
-	@classmethod
-	def create(cls, node):
-		if IGraphNodeNeo4j.providedBy(node):
-			result = node
-		elif IGraphNode.providedBy(node):
-			result = Neo4jNode(id=node.id, uri=node.uri,
-							   label=node.label,
-							   properties=dict(node.properties))
-		elif INeo4jNode.providedBy(node):
-			remote_node = remote(node) or node
-			labels = list(node.labels() or ())
-			result = Neo4jNode(id=unicode(remote_node._id),
-							   uri=unicode(remote_node.uri.string),
-							   label=labels[0] if labels else None)
-			result.label = labels[0] if labels else None
-			result.properties = dict(node) # node.properties is deprecated
-			result.neo = node
-		else:
-			result = None
-		return result
+    @classmethod
+    def create(cls, node):
+        result = None
+        if IGraphNodeNeo4j.providedBy(node):
+            result = node
+        elif IGraphNode.providedBy(node):
+            result = Neo4jNode(id=node.id,
+                               label=node.label,
+                               properties=dict(node.properties or {}))
+        elif INeo4jNode.providedBy(node):
+            labels = list(node.labels or ())
+            result = Neo4jNode(id=node.id)
+            # pylint: disable=attribute-defined-outside-init
+            result.label = labels[0] if labels else None
+            result.properties = dict(node.properties or {})
+            result.neo = node
+        return result
--- src/nti/graphdb/neo4j/relationship.py
+++ src/nti/graphdb/neo4j/relationship.py
@@ -4,14 +4,11 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import interface
-
-from py2neo.types import remote
 
 from nti.graphdb.interfaces import IGraphRelationship
 
@@ -30,32 +27,29 @@ from nti.schema.field import SchemaConfigured
 
 from nti.schema.fieldproperty import createDirectFieldProperties
 
+logger = __import__('logging').getLogger(__name__)
+
+
 @WithRepr
 @EqHash('id')
 @interface.implementer(IGraphRelationshipNeo4j)
 class Neo4jRelationship(SchemaConfigured):
-	createDirectFieldProperties(IGraphRelationshipNeo4j)
+    createDirectFieldProperties(IGraphRelationshipNeo4j)
 
-	_v_neo = None
-	neo = alias('_v_neo')
+    _v_neo = None
 
-	@classmethod
-	def create(cls, rel):
-		if IGraphRelationshipNeo4j.providedBy(rel):
-			result = rel
-		elif IGraphRelationship.providedBy(rel):
-			result = Neo4jRelationship(id=rel.id, uri=rel.uri, type=rel.type,
-									   start=rel.start, end=rel.end,
-									   properties=dict(rel.properties))
-		elif INeo4jRelationship.providedBy(rel):
-			remote_rel = remote(rel) or rel
-			result = Neo4jRelationship(type=rel.type,
-									   id=unicode(remote_rel._id),
-									   uri=unicode(remote_rel.uri.string),
-									   end=Neo4jNode.create(rel.end_node()),
-									   start=Neo4jNode.create(rel.start_node()),
-									   properties=dict(rel))
-			result.neo = rel
-		else:
-			result = None
-		return result
+    neo = alias('_v_neo')
+
+    @classmethod
+    def create(cls, rel):
+        result = None
+        if IGraphRelationshipNeo4j.providedBy(rel):
+            result = rel
+        elif INeo4jRelationship.providedBy(rel) or IGraphRelationship.providedBy(rel):
+            result = Neo4jRelationship(id=rel.id,
+                                       type=rel.type,
+                                       end=Neo4jNode.create(rel.end),
+                                       start=Neo4jNode.create(rel.start),
+                                       properties=dict(rel.properties or {}))
+            result.neo = rel if INeo4jRelationship.providedBy(rel) else None
+        return result
