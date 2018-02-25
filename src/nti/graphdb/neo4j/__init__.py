fa2c886f501e8460cd1f9b32b3e66cf8cdc8f509
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-24 18:17:52 -0600

    remove unused

--- src/nti/graphdb/neo4j/__init__.py
+++ src/nti/graphdb/neo4j/__init__.py
@@ -4,13 +4,6 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
-
-from nti.graphdb.neo4j.database import Neo4jDB
-
-from nti.graphdb.neo4j.node import Neo4jNode
-
-from nti.graphdb.neo4j.relationship import Neo4jRelationship
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
