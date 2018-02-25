bb382721e2bfe9a44fa400b5a3d8dfaefaf0ef75
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-24 18:07:37 -0600

    pylint; use proper constants

--- src/nti/graphdb/__init__.py
+++ src/nti/graphdb/__init__.py
@@ -4,45 +4,46 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import component
+
+from nti.asynchronous import create_job
+from nti.asynchronous import get_job_queue as async_queue
+
+from nti.externalization.interfaces import StandardExternalFields
+from nti.externalization.interfaces import StandardInternalFields
+
+from nti.graphdb.interfaces import IGraphDB
+from nti.graphdb.interfaces import IGraphDBQueueFactory
+
+#: OID field
+OID = StandardExternalFields.OID.lower()
+
+#: NTIID field
+NTIID = StandardInternalFields.NTIID
+
+#: IntId field
+INTID = StandardExternalFields.INTID.lower()
+
+#: CreatedTime field
+CREATED_TIME = StandardInternalFields.CREATED_TIME
+
+#: Redis queue name
+QUEUE_NAME = "++etc++graphdb++queue"
 
 logger = __import__('logging').getLogger(__name__)
-# 
-# from zope import component
-# 
-# from nti.asynchronous import create_job
-# from nti.asynchronous import get_job_queue as async_queue
-# 
-# from nti.externalization.interfaces import StandardExternalFields
-# from nti.externalization.interfaces import StandardInternalFields
-# 
-# from nti.graphdb.interfaces import IGraphDB
-# from nti.graphdb.interfaces import IGraphDBQueueFactory
-# 
-# #: OID field
-# OID = StandardExternalFields.OID.lower()
-# 
-# #: NTIID field
-# NTIID = StandardExternalFields.NTIID.lower()
-# 
-# #: IntId field
-# INTID = StandardExternalFields.INTID.lower()
-# 
-# #: CreatedTime field
-# CREATED_TIME = StandardInternalFields.CREATED_TIME
-# 
-# #: Redis queue name
-# QUEUE_NAME = "++etc++graphdb++queue"
-# 
-# 
-# def get_graph_db():
-#     return component.queryUtility(IGraphDB)
-# 
-# 
-# def get_factory():
-#     return component.getUtility(IGraphDBQueueFactory)
-# 
-# 
-# def get_job_queue(name=QUEUE_NAME):
-#     return get_factory().get_queue(name)
+
+
+def get_graph_db():
+    return component.queryUtility(IGraphDB)
+
+
+def get_factory():
+    return component.getUtility(IGraphDBQueueFactory)
+
+
+def get_job_queue(name=QUEUE_NAME):
+    return get_factory().get_queue(name)
