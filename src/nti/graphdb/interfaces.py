13ee97b9a614c631c7527a97de6a52d0c63bd51b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-24 20:22:55 -0600

    pylint

--- src/nti/graphdb/interfaces.py
+++ src/nti/graphdb/interfaces.py
@@ -4,8 +4,11 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=inherit-non-class,expression-not-assigned
 
 from zope import interface
 
@@ -20,7 +23,7 @@ from nti.schema.field import List
 from nti.schema.field import Number
 from nti.schema.field import Object
 from nti.schema.field import Variant
-from nti.schema.field import ValidTextLine
+from nti.schema.field import DecodingValidTextLine as ValidTextLine
 
 #: Neo4J database name
 NEO4J = u"neo4j"
@@ -37,225 +40,271 @@ MODIFY_EVENT = 1
 #: Remove event code
 REMOVE_EVENT = 2
 
+
 class IGraphDBQueueFactory(interface.Interface):
-	"""
-	A factory for graphdb processing queues.
-	"""
+    """
+    A factory for graphdb processing queues.
+    """
+
 
 class IGraphDB(IExternalService):
 
-	def create_node(obj, label=None, properties=None, key=None, value=None):
-		pass
+    def create_node(obj, label=None, key=None, value=None, properties=None):
+        """
+        Create a graph node for the specified object
+        
+        :param object obj: Object to create the node for
+        :param str label: Optional object label
+        :param str key: Optional object [primary] key name
+        :param str value: Optional object [primary] key value
+        :param dict properties: Optional node properties
+        """
+        pass
 
-	def create_nodes(*objs):
-		pass
+#     def create_nodes(*objs):
+#         pass
+# 
+#     def get_node(obj):
+#         pass
+# 
+#     def get_or_create_node(obj):
+#         pass
+# 
+#     def get_nodes(*objs):
+#         pass
+# 
+#     def get_indexed_node(label, key, value):
+#         pass
+# 
+#     def get_indexed_nodes(tuples):
+#         pass
+# 
+#     def get_node_properties(obj):
+#         pass
+# 
+#     def update_node(obj, properties=None):
+#         pass
+# 
+#     def delete_node(obj):
+#         pass
+# 
+#     def delete_nodes(*objs):
+#         pass
+# 
+#     def create_relationship(start, end, rel_type, properties=None, key=None, value=None):
+#         pass
+# 
+#     def get_relationship(obj):
+#         pass
+# 
+#     def match(start=None, end=None, rel_type=None, bidirectional=False,
+#               limit=None, loose=False):
+#         pass
+# 
+#     def delete_relationships(*rels):
+#         pass
+# 
+#     def update_relationship(obj, properties=None):
+#         pass
+# 
+#     def find_relationships(key, value, rel_type=None, start=None, end=None,
+#                            bidirectional=False):
+#         """
+#         Find relationships for the specified key and value property
+#         """
+# 
+#     def index_relationship(rel, key, value):
+#         pass
+# 
+#     def get_indexed_relationships(key, value):
+#         pass
+# 
+#     def unindex_relationship(key, value, rel=None):
+#         pass
 
-	def get_node(obj):
-		pass
-
-	def get_or_create_node(obj):
-		pass
-
-	def get_nodes(*objs):
-		pass
-
-	def get_indexed_node(label, key, value):
-		pass
-
-	def get_indexed_nodes(tuples):
-		pass
-
-	def get_node_properties(obj):
-		pass
-
-	def update_node(obj, properties=None):
-		pass
-
-	def delete_node(obj):
-		pass
-
-	def delete_nodes(*objs):
-		pass
-
-	def create_relationship(start, end, rel_type, properties=None, key=None, value=None):
-		pass
-
-	def get_relationship(obj):
-		pass
-
-	def match(start=None, end=None, rel_type=None, bidirectional=False,
-			  limit=None, loose=False):
-		pass
-
-	def delete_relationships(*rels):
-		pass
-
-	def update_relationship(obj, properties=None):
-		pass
-
-	def find_relationships(key, value, rel_type=None, start=None, end=None,
-						   bidirectional=False):
-		"""
-		Find relationships for the specified key and value property
-		"""
-
-	def index_relationship(rel, key, value):
-		pass
-
-	def get_indexed_relationships(key, value):
-		pass
-
-	def unindex_relationship(key, value, rel=None):
-		pass
 
 class INode(interface.Interface):
-	"""
-	Marker interface for a node
-	"""
+    """
+    Marker interface for a node
+    """
+
 
 class IGraphNode(INode):
-	id = ValidTextLine(title="node id")
-	label = ValidTextLine(title="label", required=False)
-	uri = ValidTextLine(title="uri identifier", required=False)
-	properties = Dict(ValidTextLine(title="The key"),
-					  Variant((Number(title="Number value"),
-							   Bool(title='Boolean value'),
-							   ValidTextLine(title='String value')), title="The value"),
-							  required=False,
-							  min_length=0)
+    id = ValidTextLine(title=u"node id")
+
+    label = ValidTextLine(title=u"label", required=False)
+
+    properties = Dict(ValidTextLine(title=u"The key"),
+                      Variant((Number(title=u"The Number"),
+                               Bool(title=u'The Boolean'),
+                               ValidTextLine(title=u'The String value')), 
+                               title=u"The value"),
+                      required=False,
+                      min_length=0)
+
 
 class IObjectProcessor(interface.Interface):
 
-	def init(db, obj):
-		"""
-		build relationships for the specified object
-		"""
+    def init(db, obj):
+        """
+        build relationships for the specified object
+        """
+
 
 class IRelationshipType(interface.Interface):
-	"""
-	Marker interface for a relationship type
-	"""
+    """
+    Marker interface for a relationship type
+    """
 
-	def __str__():
-		pass
+    def __str__():
+        pass
+
 
 class IRelationship(interface.Interface):
-	"""
-	Marker interface for a relationship
-	"""
+    """
+    Marker interface for a relationship
+    """
+
 
 class IGraphRelationship(IRelationship):
 
-	id = ValidTextLine(title="relationship id")
+    id = ValidTextLine(title=u"relationship id")
 
-	uri = ValidTextLine(title="uri identifier", required=False)
+    type = Variant((Object(IRelationshipType),
+                    ValidTextLine(title=u'relationship type')),
+                   title=u"The relationship type")
 
-	type = Variant((Object(IRelationshipType, description="A :class:`.Interface`"),
-					ValidTextLine(title='relationship type')),
-					title="The relationship type")
+    start = Object(INode, title=u"The start node", required=False)
 
-	start = Object(INode, title="The start node", required=False)
+    end = Object(INode, title=u"The end node", required=False)
 
-	end = Object(INode, title="The end node", required=False)
+    properties = Dict(ValidTextLine(title=u"The key"),
+                      Variant((ValidTextLine(title=u"The string"),
+                               Number(title=u"The number"),
+                               Bool(title=u"The bool"),
+                               List(title=u"The list"))),
+                      required=False)
 
-	properties = Dict(ValidTextLine(title="key"),
-					  Variant((ValidTextLine(title="value string"),
-							   Number(title="value number"),
-							   Bool(title="value bool"),
-							   List(title="value list"))),
-					  required=False)
 
 class IPropertyAdapter(IDict):
-	"""
-	marker interface for object properties
-	"""
+    """
+    marker interface for object properties
+    """
+
 
 class ILabelAdapter(IString):
-	"""
-	marker interface for an object label
-	"""
+    """
+    marker interface for an object label
+    """
+
 
 class IUniqueAttributeAdapter(interface.Interface):
-	"""
-	Interface to specify the attribute name/value that uniquely identifies an object
-	"""
-	key = interface.Attribute("Attribute key")
-	value = interface.Attribute("Attribute value")
+    """
+    Interface to specify the attribute name/value that uniquely identifies an object
+    """
+    key = interface.Attribute("Attribute key")
+    value = interface.Attribute("Attribute value")
 
-class IFriendOf(IRelationshipType):
-	pass
-
-class IMemberOf(IRelationshipType):
-	pass
-
-class IFollow(IRelationshipType):
-	pass
-
-class IFlagged(IRelationshipType):
-	pass
-
-class ICreated(IRelationshipType):
-	pass
-
-class ILike(IRelationshipType):
-	pass
-
-class IRate(IRelationshipType):
-	pass
-
-class IAuthor(IRelationshipType):
-	pass
-
-class IShared(IRelationshipType):
-	pass
-
-class IIsSharedWith(IRelationshipType):
-	pass
-
-class ICommentOn(IRelationshipType):
-	pass
-
-class ITaggedTo(IRelationshipType):
-	pass
-
-class IViewed(IRelationshipType):
-	pass
-
-class ITakenAssessment(IRelationshipType):
-	pass
-
-class ITakenInquiry(IRelationshipType):
-	pass
 
 class IContainer(interface.Interface):
-	pass
+    pass
+
+
+class IFriendOf(IRelationshipType):
+    pass
+
+
+class IMemberOf(IRelationshipType):
+    pass
+
+
+class IFollow(IRelationshipType):
+    pass
+
+
+class IFlagged(IRelationshipType):
+    pass
+
+
+class ICreated(IRelationshipType):
+    pass
+
+
+class ILike(IRelationshipType):
+    pass
+
+
+class IRate(IRelationshipType):
+    pass
+
+
+class IAuthor(IRelationshipType):
+    pass
+
+
+class IShared(IRelationshipType):
+    pass
+
+
+class IIsSharedWith(IRelationshipType):
+    pass
+
+
+class ICommentOn(IRelationshipType):
+    pass
+
+
+class ITaggedTo(IRelationshipType):
+    pass
+
+
+class IViewed(IRelationshipType):
+    pass
+
+
+class ITakenAssessment(IRelationshipType):
+    pass
+
+
+class ITakenInquiry(IRelationshipType):
+    pass
+
 
 class IContained(IRelationshipType):
-	pass
+    pass
+
 
 class IFeedback(IRelationshipType):
-	pass
+    pass
+
 
 class IAssignmentFeedback(IRelationshipType):
-	pass
+    pass
+
 
 class IIsReplyOf(IRelationshipType):
-	pass
+    pass
+
 
 class IReply(IRelationshipType):
-	pass
+    pass
+
 
 class IParentOf(IRelationshipType):
-	pass
+    pass
+
 
 class IBelong(IRelationshipType):
-	pass
+    pass
+
 
 class IEnroll(IRelationshipType):
-	pass
+    pass
+
 
 class IUnenroll(IRelationshipType):
-	pass
+    pass
+
 
 class IBought(IRelationshipType):
-	pass
+    pass
