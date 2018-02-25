3e6e4dd41c13733b2a6de9d25f8be104714d4064
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-25 15:03:32 -0600

    pylint;pep8

--- src/nti/graphdb/unique.py
+++ src/nti/graphdb/unique.py
@@ -4,10 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import component
 from zope import interface
@@ -39,104 +38,116 @@ from nti.graphdb.common import get_ntiid
 from nti.graphdb.interfaces import IContainer
 from nti.graphdb.interfaces import IUniqueAttributeAdapter
 
+logger = __import__('logging').getLogger(__name__)
+
 
 @WithRepr
 @component.adapter(interface.Interface)
 @interface.implementer(IUniqueAttributeAdapter)
 class _GenericUniqueAttributeAdpater(object):
 
-	key = value = None
+    key = value = None
 
-	def __init__(self, obj):
-		self.obj = obj
+    def __init__(self, obj):
+        self.obj = obj
+
 
 @interface.implementer(IUniqueAttributeAdapter)
 class _IntIDUniqueAttributeAdpater(_GenericUniqueAttributeAdpater):
 
-	key = INTID
+    key = INTID
 
-	@Lazy
-	def value(self):
-		intids = component.queryUtility(IIntIds)
-		result = intids.queryId(self.obj) if intids is not None else None
-		return result
+    @Lazy
+    def value(self):
+        intids = component.queryUtility(IIntIds)
+        result = intids.queryId(self.obj) if intids is not None else None
+        return result
+
 
 @component.adapter(IEntity)
 @interface.implementer(IUniqueAttributeAdapter)
 class _EntityUniqueAttributeAdpater(_GenericUniqueAttributeAdpater):
 
-	key = "username"
+    key = u"username"
 
-	@Lazy
-	def value(self):
-		if IUseNTIIDAsExternalUsername.providedBy(self.obj):
-			return get_ntiid(self.obj) or get_oid(self.obj)
-		return self.obj.username
+    @Lazy
+    def value(self):
+        if IUseNTIIDAsExternalUsername.providedBy(self.obj):
+            return get_ntiid(self.obj) or get_oid(self.obj)
+        return self.obj.username
+
 
 @interface.implementer(IUniqueAttributeAdapter)
 @component.adapter(IDynamicSharingTargetFriendsList)
 class _DFLUniqueAttributeAdpater(_GenericUniqueAttributeAdpater):
 
-	key = "username"
+    key = u"username"
 
-	@Lazy
-	def value(self):
-		return get_ntiid(self.obj) or get_oid(self.obj)
+    @Lazy
+    def value(self):
+        return get_ntiid(self.obj) or get_oid(self.obj)
+
 
 @interface.implementer(IUniqueAttributeAdapter)
 class _OIDUniqueAttributeAdpater(_GenericUniqueAttributeAdpater):
 
-	key = OID
+    key = OID
 
-	@Lazy
-	def value(self):
-		result = get_oid(self.obj)
-		return result
+    @Lazy
+    def value(self):
+        result = get_oid(self.obj)
+        return result
 OIDUniqueAttributeAdpater = _OIDUniqueAttributeAdpater  # BWC
+
 
 @component.adapter(ITopic)
 @interface.implementer(IUniqueAttributeAdapter)
 class _TopicUniqueAttributeAdpater(_OIDUniqueAttributeAdpater):
 
-	@Lazy
-	def value(self):
-		return get_oid(self.obj)
+    @Lazy
+    def value(self):
+        return get_oid(self.obj)
+
 
 @component.adapter(IHeadlinePost)
 @interface.implementer(IUniqueAttributeAdapter)
 class _HeadlinePostUniqueAttributeAdpater(_TopicUniqueAttributeAdpater):
 
-	def __init__(self, obj):
-		super(_HeadlinePostUniqueAttributeAdpater, self).__init__(obj.__parent__)
+    def __init__(self, obj):
+        super(_HeadlinePostUniqueAttributeAdpater, self).__init__(obj.__parent__)
+
 
 @component.adapter(IContainer)
 @interface.implementer(IUniqueAttributeAdapter)
 class _ContainerUniqueAttributeAdpater(_OIDUniqueAttributeAdpater):
 
-	@property
-	def value(self):
-		return self.obj.id
+    @property
+    def value(self):
+        return self.obj.id
+
 
 @interface.implementer(IUniqueAttributeAdapter)
 class _NTIIDUniqueAttributeAdpater(_GenericUniqueAttributeAdpater):
 
-	key = NTIID
+    key = NTIID
 
-	@Lazy
-	def value(self):
-		return get_ntiid(self.obj)
+    @Lazy
+    def value(self):
+        return get_ntiid(self.obj)
+
 
 @component.adapter(IContentUnit)
 @interface.implementer(IUniqueAttributeAdapter)
 class _ContentUnitAttributeAdpater(_NTIIDUniqueAttributeAdpater):
 
-	@Lazy
-	def value(self):
-		return self.obj.ntiid
+    @Lazy
+    def value(self):
+        return self.obj.ntiid
+
 
 @interface.implementer(IPresentationAsset)
 class _PresentationAssetUniqueAttributeAdpater(_NTIIDUniqueAttributeAdpater):
 
-	@Lazy
-	def value(self):
-		return get_ntiid(self.obj)
+    @Lazy
+    def value(self):
+        return get_ntiid(self.obj)
