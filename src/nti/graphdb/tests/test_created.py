1d0f7376cda3b224714b0e9b1064d03256374b58
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-08 21:13:26 -0600

    enable created items

--- src/nti/graphdb/tests/test_created.py
+++ src/nti/graphdb/tests/test_created.py
@@ -1,85 +1,70 @@
-# #!/usr/bin/env python
-# # -*- coding: utf-8 -*-
-# 
-# from __future__ import print_function, unicode_literals, absolute_import, division
-# __docformat__ = "restructuredtext en"
-# 
-# # disable: accessing protected members, too many methods
-# # pylint: disable=W0212,R0904
-# 
-# from hamcrest import is_
-# from hamcrest import none
-# from hamcrest import is_not
-# from hamcrest import has_length
-# from hamcrest import assert_that
-# 
-# import unittest
-# 
-# from nti.dataserver.users import User
-# 
-# from nti.dataserver.contenttypes import Note
-# 
-# from nti.graphdb.relationships import Created
-# from nti.graphdb.neo4j.database import Neo4jDB
-# 
-# from nti.graphdb.common import get_oid
-# from nti.graphdb.common import get_node_pk
-# from nti.graphdb.created import _remove_created
-# from nti.graphdb.created import _update_created
-# from nti.graphdb.created import _add_created_relationship
-# 
-# from nti.ntiids.ntiids import make_ntiid
-# 
-# import nti.dataserver.tests.mock_dataserver as mock_dataserver
-# from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
-# 
-# from nti.graphdb.tests import DEFAULT_URI
-# from nti.graphdb.tests import cannot_connect
-# from nti.graphdb.tests import random_username
-# from nti.graphdb.tests import GraphDBTestCase
-# 
-# @unittest.skipIf(cannot_connect(DEFAULT_URI), "Neo4j not available")
-# class TestCreated(GraphDBTestCase):
-# 
-# 	def setUp(self):
-# 		super(TestCreated, self).setUp()
-# 		self.db = Neo4jDB(DEFAULT_URI)
-# 
-# 	def _create_user(self, username='nt@nti.com', password='temp001'):
-# 		usr = User.create_user(self.ds, username=username, password=password)
-# 		return usr
-# 
-# 	def _create_random_user(self):
-# 		username = random_username()
-# 		user = self._create_user(username)
-# 		return user
-# 
-# 	def _create_note(self, msg, creator, containerId=None):
-# 		note = Note()
-# 		note.body = [unicode(msg)]
-# 		note.creator = creator
-# 		note.containerId = containerId or make_ntiid(nttype='bleach', specific='manga')
-# 		return note
-# 
-# 	@WithMockDSTrans
-# 	def test_flagging(self):
-# 		user = self._create_random_user()
-# 		note = self._create_note('sample note', user)
-# 		mock_dataserver.current_transaction.add(note)
-# 		note = user.addContainedObject(note)
-# 		
-# 		oid = get_oid(note)
-# 		m = _add_created_relationship(self.db, user.username, oid)
-# 		assert_that(m, is_not(none()))
-# 
-# 		rels = self.db.match(user, note, Created())
-# 		assert_that(rels, has_length(1))
-# 		
-# 		m = _update_created(self.db, oid)
-# 		assert_that(m, is_not(none()))
-# 		
-# 		pk = get_node_pk(note)
-# 		_remove_created(self.db, pk.label, pk.key, pk.value)
-# 		
-# 		n = self.db.get_indexed_node(pk.label, pk.key, pk.value)
-# 		assert_that(n, is_(none()))
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=protected-access,too-many-public-methods,arguments-differ
+
+from hamcrest import has_length
+from hamcrest import assert_that
+
+import unittest
+
+from zope import lifecycleevent
+
+from nti.dataserver.contenttypes.note import Note
+
+from nti.dataserver.users.users import User
+
+from nti.dataserver.tests import mock_dataserver
+
+from nti.graphdb import get_graph_db
+
+from nti.graphdb.relationships import Created
+
+from nti.graphdb.tests import cannot_connect
+from nti.graphdb.tests import random_username
+
+from nti.graphdb.tests import GraphDBTestCase
+
+from nti.ntiids.ntiids import make_ntiid
+
+
+@unittest.skipIf(cannot_connect(), "Neo4j not available")
+class TestCreated(GraphDBTestCase):
+
+    def _create_user(self, username=u'nt@nti.com', password=u'temp001'):
+        usr = User.create_user(username=username, password=password)
+        return usr
+
+    def create_random_user(self):
+        user = self._create_user(random_username())
+        return user
+
+    def create_note(self, msg, creator):
+        note = Note()
+        note.body = [msg]
+        note.creator = creator
+        note.containerId = make_ntiid(nttype=u'bleach', specific=u'manga')
+        return note
+
+    @property
+    def transaction(self):
+        return mock_dataserver.current_transaction
+
+    @mock_dataserver.WithMockDSTrans
+    def test_events(self):
+        user = self.create_random_user()
+        note = self.create_note(u'sample note', user)
+        self.transaction.add(note)
+        note = user.addContainedObject(note)
+        # create and modified
+        lifecycleevent.created(note)
+        lifecycleevent.modified(note)
+        db = get_graph_db()
+        assert_that(db.match(user, note, Created()),
+                    has_length(1))
+        # remove
+        user.deleteContainedObject(note.containerId, note.id)
