9ff46d4c89129bb96bd85553c25d8f112a07e857
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-14 17:36:44 -0500

    make method public; pylint

--- src/nti/graphdb/flagging.py
+++ src/nti/graphdb/flagging.py
@@ -32,19 +32,19 @@ from nti.graphdb.relationships import Flagged
 logger = __import__('logging').getLogger(__name__)
 
 
-def _add_flagged_relationship(db, username, oid):
+def add_flagged_relationship(db, username, oid):
     result = None
     author = get_entity(username)
     obj = find_object_with_ntiid(oid)
-    if (	 obj is not None
-          and author is not None
-          and not db.match(author, obj, Flagged())):
+    if        obj is not None \
+        and author is not None \
+        and not db.match(author, obj, Flagged()):
         result = db.create_relationship(author, obj, Flagged())
         logger.debug("Flagged relationship %s created", result)
     return result
 
 
-def _remove_flagged_relationship(db, username, oid):
+def remove_flagged_relationship(db, username, oid):
     result = False
     author = get_entity(username)
     obj = find_object_with_ntiid(oid)
@@ -57,32 +57,32 @@ def _remove_flagged_relationship(db, username, oid):
     return result
 
 
-def _process_flagging_event(db, flaggable, username=None, is_flagged=True):
+def process_flagging_event(db, flaggable, username=None, is_flagged=True):
     username = username or get_current_principal_id()
     if username:
         oid = get_oid(flaggable)
         queue = get_job_queue()
         if is_flagged:
-            job = create_job(_add_flagged_relationship, db=db, username=username,
+            job = create_job(add_flagged_relationship, db=db, username=username,
                              oid=oid)
         else:
-            job = create_job(_remove_flagged_relationship, db=db, username=username,
+            job = create_job(remove_flagged_relationship, db=db, username=username,
                              oid=oid)
         queue.put(job)
 
 
 @component.adapter(IFlaggable, IObjectFlaggedEvent)
-def _object_flagged(flaggable, event):
+def _object_flagged(flaggable, unused_event):
     db = get_graph_db()
     if db is not None:
-        _process_flagging_event(db, flaggable)
+        process_flagging_event(db, flaggable)
 
 
 @component.adapter(IFlaggable, IObjectUnflaggedEvent)
-def _object_unflagged(flaggable, event):
+def _object_unflagged(flaggable, unused_event):
     db = get_graph_db()
     if db is not None:
-        _process_flagging_event(db, flaggable, is_flagged=False)
+        process_flagging_event(db, flaggable, is_flagged=False)
 
 
 component.moduleProvides(IObjectProcessor)
@@ -92,14 +92,18 @@ def init(db, obj):
     result = False
     if IFlaggable.providedBy(obj):
         store = IGlobalFlagStorage(obj)
+        # pylint: disable=too-many-function-args
         if store.is_flagged(obj):
             creator = getattr(obj, 'creator', None)
             # asume all sharedWith users have flagged object
             flaggers = list(getattr(obj, 'sharedWith', None) or ())
             if not flaggers and creator:
                 flaggers.append(creator)
-            flaggers = {getattr(x, 'username', x) for x in flaggers}
+            # get unique flaggers
+            flaggers = {
+				getattr(x, 'username', x) for x in flaggers
+			}
             for flagger in flaggers:
-                _process_flagging_event(db, obj, flagger)
+                process_flagging_event(db, obj, flagger)
             result = len(flaggers) > 0
     return result
